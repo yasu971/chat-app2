c77af809f6c8e9a136a215ec56c18b2301e4aace
yasu971 <yasuishii1421@gmail.com>
2021-03-21 10:27:02 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,16 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])    
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
 end
